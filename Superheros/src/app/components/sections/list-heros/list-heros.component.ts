0eda6d800dad0b92a86351e7bdacf08687484776
Angel <angel.jimenez@w2m.com>
2025-07-11 14:01:10 +0200

    Inicio de la lógica del viewMore

--- Superheros/src/app/components/sections/list-heros/list-heros.component.ts
+++ Superheros/src/app/components/sections/list-heros/list-heros.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { HeroCardComponent } from "../../components/hero-card/hero-card.component";
 import { HeroRequestsService } from '../../../services/hero-requests.service';
 import { Hero } from '../../../models/hero';
@@ -16,16 +16,28 @@ export class ListHerosComponent implements OnInit {
   private readonly heroesToDisplay: number = 8;
   private index: number = 0;
 
+  protected visibleHeroes = signal<Hero[]>([]);
+  protected viewMore = signal<boolean>(false);
+
   public initHeroList(): void {
     this._heroRequestsService.getAllHeroes();
   }
 
   ngOnInit(): void {
     this.initHeroList();
+    this.loadMoreHeroes();
   }
 
   loadMoreHeroes(): void {
+    const allHeroes = this.heroList;
+    const nextIndex = this.index + this.heroesToDisplay;
 
+    this.visibleHeroes.set([
+      ...this.visibleHeroes(),
+      ...allHeroes.slice(this.index, nextIndex)
+    ]);
+
+    this.index = nextIndex;
   }
 
   public get heroList(): Hero[] {
