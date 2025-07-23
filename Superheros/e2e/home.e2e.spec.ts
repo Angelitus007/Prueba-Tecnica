a3eaf70da5a85f498178e7fc321f303348f2d42b
Angel <angel.jimenez@w2m.com>
2025-07-23 09:01:32 +0200

    Testing E2E terminado, añadidas las carpetas de playwright reports y de tests results al gitignore.

--- Superheros/e2e/home.e2e.spec.ts
+++ Superheros/e2e/home.e2e.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { Hero } from '../src/app/models/hero';
 
+// Para ejecutarlos: npm run e2e
+
 test.describe('Page Home - Tests E2E', () => {
 
   test.beforeEach(async ({ page }) => {
@@ -34,7 +36,7 @@ test.describe('Page Home - Tests E2E', () => {
     await expect(noHeroesMessage).toBeVisible();
   });
 
-  test.skip('Debería poder crear un nuevo héroe', async ({ page }) => {
+  test.only('Debería poder crear un nuevo héroe correctamente', async ({ page }) => {
     // GIVEN
     const createHeroButton = page.locator('section-create-hero button');
     const hero: Omit<Hero, 'id' | 'imageURL'> = {
@@ -58,9 +60,8 @@ test.describe('Page Home - Tests E2E', () => {
     const checkbox = dialogHeroCreation.locator('input[formControlName="terms"]');
     await checkbox.check();
 
-    // No se me abre la imagen --->
     const photoInput = dialogHeroCreation.locator('input[formControlName="photo"]');
-    await photoInput.setInputFiles('./Server/assets/superman.jpg');
+    await photoInput.setInputFiles('../../Prueba Tecnica/Server/assets/spiderman.jpg');
 
     const createButton = dialogHeroCreation.locator('button[type="submit"]');
     await createButton.click();
@@ -70,7 +71,7 @@ test.describe('Page Home - Tests E2E', () => {
     await expect(successMessage).toBeVisible();
   });
 
-  test.only('Debería eliminar un héroe correctamente', async ({ page }) => {
+  test('Debería poder eliminar un héroe correctamente', async ({ page }) => {
     // GIVEN
     const heroListSection = page.locator('section-list-heros');
     await expect(heroListSection).toBeVisible();
@@ -91,8 +92,70 @@ test.describe('Page Home - Tests E2E', () => {
     await expect(successMessage).toBeVisible();
   });
 
-  test('Debería alternar entre "Ver más" y "Ver menos"', async ({ page }) => {
+  test('Debería poder editar un héroe correctamente', async ({ page }) => {
     // GIVEN
+    const heroListSection = page.locator('section-list-heros');
+    await expect(heroListSection).toBeVisible();
+
+    const heroCard = heroListSection.locator('c-hero-card').first();
+    await expect(heroCard).toBeVisible();
+
+    const editButton = heroCard.locator('button.hero-card__b-editar');
+    await expect(editButton).toBeVisible();
+
+    // WHEN
+    await editButton.click();
+
+    const dialogHeroEdit = page.locator('c-dialog');
+    await expect(dialogHeroEdit).toBeVisible();
+
+    const heroNameInput = dialogHeroEdit.locator('input[formControlName="name"]');
+    await heroNameInput.fill('Héroe Editado');
+
+    const checkbox = dialogHeroEdit.locator('input[formControlName="terms"]');
+    await checkbox.check();
+
+    const createButton = dialogHeroEdit.locator('button[type="submit"]');
+    await createButton.click();
+
+    // THEN
+    const successMessage = page.locator('text=Héroe modificado con éxito');
+    await expect(successMessage).toBeVisible();
+
+  });
+
+  // Estos 3 tests los he generado interacturando directamente con la página, usando el generador de tests de Playwright.
+  // El comando es: npx playwright codegen http://localhost:4200 -->
+  test('Debería alternar entre "Ver más" y "Ver menos"', async ({ page }) => {
+
+    await page.getByRole('button', { name: 'Ver más' }).click();
+    await page.getByRole('button', { name: 'Ver más' }).click();
+    await page.getByRole('button', { name: 'Ver menos' }).click();
+    await page.getByRole('button', { name: 'Ver menos' }).click();
+  });
+
+  test('Debería poder navegar a la página de Terms a través de crear héroe', async ({ page }) => {
+
+    await page.getByRole('button', { name: 'Add Icon Crear nuevo héroe' }).click();
+
+    const pagePromise = page.waitForEvent('popup');
+    await page.getByRole('link', { name: 'Términos y condiciones' }).click();
+
+    const page1 = await pagePromise;
+    await expect(page1).toHaveURL('http://localhost:4200/terms');
+
+  });
+
+  test('Debería poder navegar a la página de Terms a través de editar héroe', async ({ page }) => {
+
+    await page.locator('c-hero-card').filter({ hasText: 'Batman Genio detective,' }).getByRole('button').first().click();
+
+    const pagePromise = page.waitForEvent('popup');
+    await page.getByRole('link', { name: 'Términos y condiciones' }).click();
+
+    const page1 = await pagePromise;
+    await expect(page1).toHaveURL('http://localhost:4200/terms');
+
   });
 
 });
