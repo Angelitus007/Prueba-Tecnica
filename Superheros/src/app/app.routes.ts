96718c4d264be021544221e33f399586673745b8
Angel <angel.jimenez@w2m.com>
2025-07-24 12:39:34 +0200

    Configuración de un metaresolver para los metatags de los diferentes paths

--- Superheros/src/app/app.routes.ts
+++ Superheros/src/app/app.routes.ts
@@ -1,8 +1,34 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { TermsComponent } from './components/pages/terms/terms.component';
+import { MetaResolver } from '../resolvers/meta-resolver';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, title: 'Home' },
-  { path: 'terms', component: TermsComponent, title: 'Términos y Condiciones' }
+  { path: '',
+    component: HomeComponent,
+    title: 'Home',
+    data: {
+      description: 'This is our Home Page',
+      robots: 'index, follow',
+      ogTitle: 'Superheroes Home Page',
+      ogDescription: 'Discover our Superheroes Home Page',
+      ogImage: '../../public/assets/superhero_header.png',
+      ogUrl: 'http://localhost:4200/'
+    },
+    resolve: {
+      meta: MetaResolver
+    }
+  },
+  { path: 'terms',
+    component: TermsComponent,
+    title: 'Términos y Condiciones',
+    data: {
+      description: 'This is our Terms Page',
+      robots: 'index, follow', // También se puede poner noindex, nofollow
+      // No nos interesan los og aquí
+    },
+    resolve: {
+      meta: MetaResolver
+    }
+  }
 ];
---
+++ Superheros/src/app/services/meta.service.ts
@@ -0,0 +1,28 @@
+import { inject, Injectable } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MetaService {
+
+  private readonly meta = inject(Meta)
+
+  public setMetaTags(description: string, robots: string, ogTitle?: string, ogDescription?: string, ogImage?: string, ogUrl?: string): void {
+    this.meta.updateTag({ name: 'description', content: description });
+    this.meta.updateTag({ name: 'robots', content: robots });
+
+    const ogTags = {
+      'og:title': ogTitle,
+      'og:description': ogDescription,
+      'og:image': ogImage,
+      'og:url': ogUrl
+    };
+
+    Object.entries(ogTags).forEach(([property, content]) => {
+      if (content) {
+        this.meta.updateTag({ property, content });
+      }
+    });
+  }
+}
