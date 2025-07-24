4bc785c9b956689a5c74d9e3cda55506298d8e42
Angel <angel.jimenez@w2m.com>
2025-07-24 11:10:54 +0200

    Config para poder tirar con SSR, y poder hacer build y : ng build && ng run Superheros:server

--- Superheros/src/app/app.routes.server.ts
+++ Superheros/src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'terms',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: '**', // Otras rutas
+    renderMode: RenderMode.Server
   }
 ];
